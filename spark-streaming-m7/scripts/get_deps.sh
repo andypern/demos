25a47df0a9707db86a3b4a12f895a965087d15eb
Andy Pernsteiner <andypern@gmail.com>
2014-05-22 19:04:40 -0400

    fixed deps script paths

--- spark-streaming-m7/scripts/get_deps.sh
+++ spark-streaming-m7/scripts/get_deps.sh
@@ -6,6 +6,7 @@ cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
 cd /tmp
 wget http://www.scala-lang.org/files/archive/scala-2.10.3.rpm
 rpm -ivh /tmp/scala-2.10.3.rpm
+rm -f /tmp/scala-2.10.3.rpm
 
 yum install -y lsof
 
@@ -18,9 +19,9 @@ yum install -y mapr-spark-master
  maprcli node services -name hs2 -action stop -nodes maprdemo
 
 
-cp -f /mapr/maprdemo/demos/spark-streaming-m7/conf/hive-site.xml /opt/mapr/hive/hive-0.12/conf/hive-site.xml
-cp -f /mapr/maprdemo/demos/spark-streaming-m7/conf/shark-env.sh /opt/mapr/shark/shark-0.90/conf/shark-env.sh
-cp -f /mapr/maprdemo/demos/spark-streaming-m7/conf/run /opt/mapr/shark/shark-0.90/run
+cp -f /mapr/demo.mapr.com/demos/spark-streaming-m7/conf/hive-site.xml /opt/mapr/hive/hive-0.12/conf/hive-site.xml
+cp -f /mapr/demo.mapr.com/demos/spark-streaming-m7/conf/shark-env.sh /opt/mapr/shark/shark-0.9.0/conf/shark-env.sh
+cp -f /mapr/demo.mapr.com/demos/spark-streaming-m7/conf/run /opt/mapr/shark/shark-0.9.0/run
 
 
 mkdir -p /mapr/demo.mapr.com/ingest
