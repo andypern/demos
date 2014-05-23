2a57bd3a16713c524440934c9298a350b325e656
Andy Pernsteiner <andypern@gmail.com>
2014-05-23 10:40:42 -0400

    fixed some dummy maneuver on get_deps.sh

--- spark-streaming-m7/scripts/env.sh
+++ spark-streaming-m7/scripts/env.sh
@@ -12,9 +12,10 @@ BATCHSECS=3 #length of spark streaming batches/DSTREAMs
 TABLENAME=/tables/sensortable 
 
 OUTFILE=/mapr/${CLUSTER}/CSV/sensor.csv
-JARFILE=/mapr/${CLUSTER}/demos/spark-streaming-m7/m7_streaming_import/target/scala-2.10/m7import_2.10-0.1-SNAPSHOT.jar
+
 BASEDIR=/mapr/${CLUSTER}/ingest
-DEMODIR=${BASEDIR}/demos/spark-streaming-m7
+DEMODIR=/mapr/${CLUSTER}/demos/spark-streaming-m7
+JARFILE=${DEMODIR}/m7_streaming_import/target/scala-2.10/m7import_2.10-0.1-SNAPSHOT.jar
 
 SOURCE_FILE=${BASEDIR}/SensorDataV5.csv
 JAVA_BIN=`which java`
