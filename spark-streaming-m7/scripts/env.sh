cba9e75c91393961efb21345954d98aa8999b531
Andy Pernsteiner <andypern@gmail.com>
2014-06-01 20:48:03 -0400

    fixed env.sh to do exports

--- spark-streaming-m7/scripts/env.sh
+++ spark-streaming-m7/scripts/env.sh
@@ -3,23 +3,23 @@
 
 #clustername below works for the sandbox, you may need to change if you are running on something else
 
-CLUSTER=demo.mapr.com
-MYHOST=`hostname`
-SPARK_URL=spark://${MYHOST}:7077
-PORT=9999 
+export CLUSTER=demo.mapr.com
+export MYHOST=`hostname`
+export SPARK_URL=spark://${MYHOST}:7077
+export PORT=9999 
 
-BATCHSECS=3 #length of spark streaming batches/DSTREAMs
-TABLENAME=/tables/sensortable 
+export BATCHSECS=3 #length of spark streaming batches/DSTREAMs
+export TABLENAME=/tables/sensortable 
 
-OUTFILE=/mapr/${CLUSTER}/CSV/sensor.csv
+export OUTFILE=/mapr/${CLUSTER}/CSV/sensor.csv
 
-BASEDIR=/mapr/${CLUSTER}/ingest
-DEMODIR=/mapr/${CLUSTER}/demos/spark-streaming-m7
-JARFILE=${DEMODIR}/m7_streaming_import/target/scala-2.10/m7import_2.10-0.1-SNAPSHOT.jar
+export BASEDIR=/mapr/${CLUSTER}/ingest
+export DEMODIR=/mapr/${CLUSTER}/demos/spark-streaming-m7
+export JARFILE=${DEMODIR}/m7_streaming_import/target/scala-2.10/m7import_2.10-0.1-SNAPSHOT.jar
 
-SOURCE_FILE=${BASEDIR}/SensorDataV5.csv
-JAVA_BIN=`which java`
-SLEEPSECS=.25 #sleep secs for data generator to pause between sending
+export SOURCE_FILE=${BASEDIR}/SensorDataV5.csv
+export JAVA_BIN=`which java`
+export SLEEPSECS=.25 #sleep secs for data generator to pause between sending
 
 
 #update path
